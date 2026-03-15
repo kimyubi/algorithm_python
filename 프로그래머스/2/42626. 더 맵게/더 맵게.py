b0d612e5a31e82f852e33cd70dcca73ad1cf0a88
김유비 <62657545+kimyubi@users.noreply.github.com>
2026-03-15 14:58:26 +0900

    [level 2] Title: 더 맵게, Time: 1904.07 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)   
+    cnt = len(scoville)
+
+    # 스코빌 지수가 k 미만인 음식이 있는 경우
+    while scoville and scoville[0] < K:
+        # 음식이 두가지 미만이면 새로운 음식을 만들어 스코빌 지수를 k 이상으로 만들 수 없다.
+        if cnt < 2:
+            return -1
+         
+        food_1 = heapq.heappop(scoville)
+        food_2 = heapq.heappop(scoville)
+        new_food = food_1 + (food_2 * 2)
+        heapq.heappush(scoville, new_food)
+        answer += 1
+        cnt -= 1
+        
+    return answer
