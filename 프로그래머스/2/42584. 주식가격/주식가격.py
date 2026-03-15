711cff5595d8dea2151e6f6a98e16ae8cf1152f7
김유비 <62657545+kimyubi@users.noreply.github.com>
2026-03-15 13:11:57 +0900

    [level 2] Title: 주식가격, Time: 26.27 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,40 @@
-from collections import deque
 def solution(prices):
-    answer = []
-    prices = deque(prices)
     
-    while prices:
-        p = prices.popleft()
-        cnt = 0
-        for others in prices:
-            cnt += 1
-            if p > others:
-                break
-        answer.append(cnt)
-            
+    answer = [0] * len(prices)
+    stack = []
+
+    for i in range(len(prices)):
+
+        # 현재 가격이 더 작으면
+        # 기다리던 사람들의 가격이 떨어진 것
+        while stack and prices[stack[-1]] > prices[i]:
+
+            past = stack.pop()
+            answer[past] = i - past
+
+        stack.append(i)
+
+    # 끝까지 가격 안 떨어진 사람들
+    while stack:
+        past = stack.pop()
+        answer[past] = len(prices) - 1 - past
+
     return answer
+
+
+# from collections import deque
+# def solution(prices):
+#     answer = []
+#     prices = deque(prices)
+    
+#     while prices:
+#         p = prices.popleft()
+#         time = 0
+#         for others in prices:
+#             time += 1
+#             # 가격이 떨어지면
+#             if p > others:
+#                 break
+#         answer.append(time)
+            
+#     return answer
