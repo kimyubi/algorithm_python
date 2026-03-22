68a2a41ed61cd8eb49bf55124373ed5ec861bca9
김유비 <62657545+kimyubi@users.noreply.github.com>
2026-03-22 10:24:20 +0900

    [level 2] Title: 타겟 넘버, Time: 368.35 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,11 @@
+def solution(numbers, target):
+    def dfs(index, current_sum):
+        # 종료 조건
+        if index == len(numbers):
+            return 1 if current_sum == target else 0
+        
+        return (
+            dfs(index + 1, current_sum + numbers[index]) +
+            dfs(index + 1, current_sum - numbers[index])
+        )
+    return dfs(0, 0)
