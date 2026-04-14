bec71f5f757f03b356378abe7281a4514b258eba
김유비 <62657545+kimyubi@users.noreply.github.com>
2026-04-14 09:17:03 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,11 +1,10 @@
 def solution(n, m):
     a, b = n, m
-
-    # 최대공약수 (유클리드 호제법)
+    
     while b != 0:
-        a, b = b, a % b
-
-    gcd = a
-    lcm = n * m // gcd
-
-    return [gcd, lcm]
+        a, b = b, a%b
+        
+    최대공약수 = a
+    최소공배수 = (n * m) // a
+    return [최대공약수, 최소공배수]
+    
