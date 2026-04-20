c6f3216bb5c29cada42736c31d36b62cdfd22822
김유비 <62657545+kimyubi@users.noreply.github.com>
2026-04-20 10:27:35 +0900

    [level 1] Title: 시저 암호, Time: 1.91 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,12 @@
+def solution(s, n):
+    answer = ""
+    
+    for ch in s:
+        if ch == " ":
+            answer += " "
+        elif ch.isupper():
+            answer += chr((ord(ch) - ord('A') + n) % 26 + ord('A'))
+        else:
+            answer += chr((ord(ch) - ord('a') + n) % 26 + ord('a'))
+    
+    return answer
